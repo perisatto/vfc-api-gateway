e9ff6f66709433aece9c01375943b105b1f7bdc4
Rodrigo Perisatto <rodrigo@perisatto.com>
2024-09-22 22:14:58 -0300

    1

--- main.tf
+++ main.tf
@@ -122,7 +122,7 @@ resource "aws_api_gateway_method" "customer_post" {
 resource "aws_api_gateway_integration" "customer_post" {
   	rest_api_id = aws_api_gateway_rest_api.main.id
   	resource_id = aws_api_gateway_resource.customers.id
-  	http_method = "POST"
+  	http_method = aws_api_gateway_method.customer_post.http_method
 
   	integration_http_method = "POST"
   	type                    = "HTTP_PROXY"
