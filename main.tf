1cc9f97401dca2b6a6d2bd2639e905bf8565a3e9
Rodrigo Perisatto <rodrigo@perisatto.com>
2024-09-22 22:33:25 -0300

    Fix

--- main.tf
+++ main.tf
@@ -110,7 +110,7 @@ resource "aws_api_gateway_resource" "customers" {
 
 resource "aws_api_gateway_method" "customer_post" {
   	rest_api_id   = aws_api_gateway_rest_api.main.id
-  	resource_id   = aws_api_gateway_resource.proxy.id
+  	resource_id   = aws_api_gateway_resource.customers.id
   	http_method   = "POST"
   	authorization = "NONE"
 
